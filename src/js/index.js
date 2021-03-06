6b98c09c0d92336de6f06a15f3b97abcd5b0bc13
i.maslakoff <igormaslakoff@gmail.com>
2021-03-06 13:00:26 +0300

    class work
    Co-authored-by: Dmitri Urbanovich <dimaurbanovich@users.noreply.github.com>
    Co-authored-by: rominaghanbari@gmail.com <rominaghanbari@gmail.com>
    Co-authored-by: Elizaveta-Battalova <Elizaveta-Battalova@users.noreply.github.com>

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+import '../styles/main.css';
+
+let $input = document.querySelector('#js-insert');
+let $taskTable = document.querySelector("#js-list")
+
+let tasks = ["Buy", "somes", "drinks"];
+
+const renderTasksList = (list) => {
+    $taskTable.innerHTML = '';
+    list.forEach((task, index) => {
+        // console.log(index, index % 2 === 0 ? 'чет' : className);
+        let className = '';
+        if(index % 2 !== 0) {
+            className = 'class = "completed"'
+        }
+        let listElement = `<li ${className}>
+        <div class="todo">
+        <input type="checkbox" class="toggle">
+        <span>${task}</span>
+        <button class="destroy"></button>
+        </div>
+        <input type="text" class="edit"></li>`;
+        $taskTable.insertAdjacentHTML("beforeend", listElement)
+    });
+}
+
+$input.addEventListener('keyup', (event) => {
+   if (event.which === 13) {
+    tasks.push($input.value)
+    $input.value = "";
+    console.log('tasks: ', tasks);   
+    renderTasksList(tasks); 
+   }
+  
+});
+
+renderTasksList(tasks);
+
+
+
+
+// <li data-id="1614959131349" class="completed"><div class="todo"><input type="checkbox" class="toggle"><span>3</span><button class="destroy"></button></div><input type="text" class="edit"></li>
+// <li data-id="1614959131961"><div class="todo"><input type="checkbox" class="toggle"><span>4</span><button class="destroy"></button></div><input type="text" class="edit"></li>
