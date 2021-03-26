550c224a5fbf6f62ce0d490be76dc89e68c8b23f
i.maslakoff <igormaslakoff@gmail.com>
2021-03-27 00:08:27 +0300

    add async app wrapper

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,57 @@
 import '../styles/main.css';
 import { ID } from './utils';
 
-
-let $input = document.querySelector('#js-insert');
-let $taskTable = document.querySelector("#js-list");
-let $counter = document.querySelector("#js-total");
-const $tasksFilter = document.querySelector('#js-filters');
-const $tasksBtnFilter = document.querySelectorAll('#js-filters > li');
-let clearButton = document.querySelector("#js-clear-completed");
-
-
-const inputLocalKey = "text";
-const selectedFilterKey = "selectedFilter";
-$input.value = localStorage.getItem(inputLocalKey);
+async function app() {
+    let $input = document.querySelector('#js-insert');
+    let $taskTable = document.querySelector("#js-list");
+    let $counter = document.querySelector("#js-total");
+    const $tasksFilter = document.querySelector('#js-filters');
+    const $tasksBtnFilter = document.querySelectorAll('#js-filters > li');
+    let clearButton = document.querySelector("#js-clear-completed");
 
 
+    const inputLocalKey = "text";
+    const selectedFilterKey = "selectedFilter";
+    $input.value = localStorage.getItem(inputLocalKey);
 
-// const convertedList = JSON.stringify(list);
-// // console.log(JSON.stringify(item));
+    // const convertedList = JSON.stringify(list);
+    // // console.log(JSON.stringify(item));
 
-// const obj = JSON.parse(text);
-// console.log(typeof item, item)
-// console.log(typeof converted, converted)
-// localStorage.setItem(
-//     'item', 
-//     converted
-//  );
+    // const obj = JSON.parse(text);
+    // console.log(typeof item, item)
+    // console.log(typeof converted, converted)
+    // localStorage.setItem(
+    //     'item', 
+    //     converted
+    //  );
 
-const keyLSTasks = 'tasks';
+    const keyLSTasks = 'tasks';
 
-let tasks = localStorage.getItem(keyLSTasks);
+    let tasks = localStorage.getItem(keyLSTasks);
 
-if (tasks === null ) {
-    tasks = [];
-} else {
-    tasks = JSON.parse(tasks);
-}
+    if (tasks === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(tasks);
+    }
 
 
-// localStorage.setItem('tasks', JSON.stringify(tasks));
+    // localStorage.setItem('tasks', JSON.stringify(tasks));
 
-const renderTasksList = (list) => {
-    hideCompletedBtn(tasks);
-    $counter.innerHTML =  `${list.length} items left ` ;
-    $taskTable.innerHTML = '';
-    list.forEach((task) => {
-        const checked = task.completed ? 'checked' : '';
+    const renderTasksList = (list) => {
+        hideCompletedBtn(tasks);
+        $counter.innerHTML = `${list.length} items left `;
+        $taskTable.innerHTML = '';
+        list.forEach((task) => {
+            const checked = task.completed ? 'checked' : '';
 
-        const liTask = document.createElement('li');
-        liTask.id = task.id;
-        if(task.completed) {
-            liTask.classList.add('completed');
-        }
+            const liTask = document.createElement('li');
+            liTask.id = task.id;
+            if (task.completed) {
+                liTask.classList.add('completed');
+            }
 
-        liTask.innerHTML = `
+            liTask.innerHTML = `
         <input ${checked} data-id="${task.id}" type="checkbox" class="toggle">
         <div class="todo">
         
@@ -62,128 +60,132 @@ const renderTasksList = (list) => {
         </div>
         <button data-value="${task.id}" class="destroy"></button>`
 
-        liTask.addEventListener('dblclick', () => {
-            liTask.classList.add('editing');
-        })
+            liTask.addEventListener('dblclick', () => {
+                liTask.classList.add('editing');
+            })
 
-        const editTask = document.createElement('input');
-        editTask.type = "text";
-        editTask.className = "edit";
-        editTask.value = task.text;
-        editTask.addEventListener('keyup', (event) => {
-            if (event.key === 'Escape') {
-                liTask.classList.remove('editing');
-            }
-            if (event.key === "Enter") {
-                liTask.classList.remove('editing');
-                task.text = editTask.value
-                renderTasksList(list);
-            }
+            const editTask = document.createElement('input');
+            editTask.type = "text";
+            editTask.className = "edit";
+            editTask.value = task.text;
+            editTask.addEventListener('keyup', (event) => {
+                if (event.key === 'Escape') {
+                    liTask.classList.remove('editing');
+                }
+                if (event.key === "Enter") {
+                    liTask.classList.remove('editing');
+                    task.text = editTask.value
+                    renderTasksList(list);
+                }
+            });
+
+            liTask.append(editTask);
+            $taskTable.append(liTask);
+
         });
 
-        liTask.append(editTask);
-        $taskTable.append(liTask);
-        
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    const hideCompletedBtn = (tasks) => {
+        const completedTask = tasks.find(task => task.completed);
+        if (completedTask) {
+            clearButton.style.display = 'inline-block';
+        } else {
+            clearButton.style.display = 'none';
+        }
+    }
+
+    $input.addEventListener('keyup', (event) => {
+        let valueToStore = $input.value;
+        if (event.key === 'Enter') {
+            tasks.push({ text: valueToStore, completed: false, id: ID() });
+            $input.value = "";
+            valueToStore = ""
+            renderTasksList(tasks);
+        }
+        localStorage.setItem(inputLocalKey, valueToStore)
     });
 
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
 
-const hideCompletedBtn = (tasks) => {
-    const completedTask = tasks.find(task => task.completed);
-    if (completedTask) {
-        clearButton.style.display = 'inline-block';
-    } else {
-        clearButton.style.display = 'none';
-    }
-}
+    // const filtered = task
+    renderTasksList(tasks);
 
-$input.addEventListener('keyup', (event) => {
-   let valueToStore = $input.value;
-   if (event.key === 'Enter') {
-      tasks.push( {text: valueToStore, completed: false, id: ID()} );
-      $input.value = ""; 
-      valueToStore = ""
-      renderTasksList(tasks); 
-   } 
-   localStorage.setItem(inputLocalKey, valueToStore)
-}); 
+    function deleteComplete(event) {
+        const deleteBtn = event.target;
+        if (deleteBtn.classList.contains('destroy')) {
+            const deleteId = deleteBtn.dataset.value;
+            tasks = tasks.filter(task => task.id !== deleteId);
+            renderTasksList(tasks);
+        }
 
-
-// const filtered = task
-renderTasksList(tasks);
-
-function deleteComplete(event) {
-    const deleteBtn = event.target;
-    if(deleteBtn.classList.contains('destroy')) {
-        const deleteId = deleteBtn.dataset.value;
-        tasks = tasks.filter(task => task.id !== deleteId);
-        renderTasksList(tasks);
+        const completeBtn = event.target;
+        if (completeBtn.classList.contains('toggle')) {
+            const changeId = completeBtn.dataset.id;
+            const task = tasks.find((el) => {
+                return el.id === changeId;
+            });
+            task.completed = !task.completed;
+            renderTasksList(tasks);
+        }
     }
 
-    const completeBtn = event.target;
-    if(completeBtn.classList.contains('toggle')) {
-        const changeId = completeBtn.dataset.id;
-        const task = tasks.find((el) => {
-            return el.id === changeId;
-        });
-        task.completed = !task.completed;
-        renderTasksList(tasks);
-    }
-}
 
-
-$taskTable.addEventListener('click', deleteComplete);
+    $taskTable.addEventListener('click', deleteComplete);
 
 
 
-$tasksFilter.addEventListener('click', (event) => {
-    const targetFilter = event.target;
-    const filterType =  targetFilter.dataset.value;
+    $tasksFilter.addEventListener('click', (event) => {
+        const targetFilter = event.target;
+        const filterType = targetFilter.dataset.value;
 
-    if (filterType) {
-      $tasksBtnFilter.forEach((filter) => {
-         if (filter.dataset.value === filterType) {
-               filter.classList.add('selected');
-         } else {
-               filter.classList.remove('selected');
-         }
-      })
-      
-      localStorage.setItem(selectedFilterKey, filterType);
+        if (filterType) {
+            $tasksBtnFilter.forEach((filter) => {
+                if (filter.dataset.value === filterType) {
+                    filter.classList.add('selected');
+                } else {
+                    filter.classList.remove('selected');
+                }
+            })
 
-      let filteredTasks = tasks; 
+            localStorage.setItem(selectedFilterKey, filterType);
 
-      if (filterType === 'active') {
-         filteredTasks = filteredTasks.filter(task => !task.completed)
-      } else if (filterType === 'completed') {
-         filteredTasks = filteredTasks.filter(task => task.completed)
-      };
+            let filteredTasks = tasks;
 
-      renderTasksList(filteredTasks); 
-   }
-    
-})
-let selectedLocalFilter = localStorage.getItem(selectedFilterKey);
+            if (filterType === 'active') {
+                filteredTasks = filteredTasks.filter(task => !task.completed)
+            } else if (filterType === 'completed') {
+                filteredTasks = filteredTasks.filter(task => task.completed)
+            };
 
-$tasksBtnFilter.forEach((filter) => {
-    if (selectedLocalFilter === filter.dataset.value) {
-        filter.classList.add('selected');
-    } else {
-        filter.classList.remove('selected');
-    }
-})
+            renderTasksList(filteredTasks);
+        }
 
-clearButton.addEventListener("click", () => {
-    tasks = tasks.filter(elem => {
-        return !elem.completed
+    })
+    let selectedLocalFilter = localStorage.getItem(selectedFilterKey);
+
+    $tasksBtnFilter.forEach((filter) => {
+        if (selectedLocalFilter === filter.dataset.value) {
+            filter.classList.add('selected');
+        } else {
+            filter.classList.remove('selected');
+        }
     })
 
-    renderTasksList(tasks); 
-});
+    clearButton.addEventListener("click", () => {
+        tasks = tasks.filter(elem => {
+            return !elem.completed
+        })
 
-window.onstorage = (ev) => {
-    tasks = JSON.parse(ev.newValue);
-    console.log(tasks);
-    renderTasksList(tasks);
-} 
+        renderTasksList(tasks);
+    });
+
+    window.onstorage = (ev) => {
+        tasks = JSON.parse(ev.newValue);
+        console.log(tasks);
+        renderTasksList(tasks);
+    }
+}
+
+
+app();
--- src/js/request.js
+++ src/js/request.js
@@ -1,173 +1,62 @@
 import 'regenerator-runtime/runtime';
 
-async function main() {
+const TODO_URL = 'http://localhost:3000/todos';
 
-    
-    const URL = 'https://jsonplaceholder.typicode.com/posts'
-
-    const response = await fetch(`${URL}/1`);
+export async function getTasksAsync() {
+    const response = await fetch(TODO_URL);
     const data = await response.json();
-    console.log(`GET data`, data);
+    return data;
+}
 
+export async function getTaskAsync(id) {
+    const res = await fetch(`${TODO_URL}/${id}`);
+    const dataRes = await res.json();
+    return dataRes;
+}
 
-    const newRecord = {
-        userId: 1, 
-        title: "our title", 
-        body: "new body"
-    };
-
-    const newRecordResponse = await fetch(URL, {
+export async function createTaskAsync(task) {
+    const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(newRecord),
-    });
 
-    const newRecordData = await newRecordResponse.json();
-    console.log(`POST data`, newRecordData);
+        body: JSON.stringify(task),
+    };
+    const newRecordResponse = await fetch(TODO_URL, options);
+    const dataRes = await newRecordResponse.json();
+    return dataRes;
 }
 
-// main();
-
-
-
-// function callback(wwww) {
-//     console.log(wwww);
-//     return wwww.json();
-// }
-
-// promise
-// .then(callback)
-// .then((posts) => {
-//     console.log(posts);
-// }))
-
-// promise
-// .then((resp) => {
-//     console.log(resp)
-// })
-
-// function sum(b) {
-//     console.log(b);
-// }
-
-// const sumArrow = (a) => {
-//     console.log(a)
-// }
-// const sumArrow2 = response => {
-//     console.log(response)
-// }
-
-
-
-// sumArrow2(1)
-// promise
-// .then( (response) => {
-//     console.log(response)
-// })
-// sum(4)
-
-
-// function fetchCustom(url) {
-//     return new Promise((resolve, rej) => {
-//         setTimeout(() => {
-//             const data = {
-//                 status: 200,
-//                 url: url,
-//                 body: {
-//                     model: 'audi',
-//                     color: 'black'
-//                 }
-//             }
-//             const err = new Error('No cars');
-//             rej(err);
-//         }, 1000);
-//     })
-// }
-
-// // AJAX  
-// // XMLHttpRequest   / 
-
-// const pr  = fetchCustom('abv.by');
-
-// pr
-// .then(resp => {
-//     console.log(resp);
-// })
-// .catch(err => {
-//     console.log('Error: ' + err.message);
-// })
-
-
-function getTasks() {
-    const getTask = fetch('http://localhost:3000/todos');
-    getTask
-    .then(promise => {
-        console.log(promise)
-        return promise.json();
-    })
-    .then(promise => {
-        console.log(promise);
-    })
-    .catch(err => {
-        console.log('Error:', err)
-    })
+export async function updateTaskAsync(task) {
+    const options = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task),
+    };
+    const url = `${TODO_URL}/${task.id}`;
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
 }
 
-const TODO_URL = 'http://localhost:3000/todos';
-async function app() {
-    
-    async function getTasksAsync() {
-        const response = await fetch(TODO_URL);
-        const data = await response.json();
-        return data;
-    }
-
-    async function getTaskAsync(id) {
-        const res = await fetch(`${TODO_URL}/${id}`);
-        const dataRes = await res.json();
-        return dataRes;
-    }
-
-    async function createTaskAsync(task) {
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-             
-            body:  JSON.stringify(task),
-        };
-        const newRecordResponse = await fetch(TODO_URL, options);
-        const dataRes = await newRecordResponse.json();
-        return dataRes;
-    }
-
-    async function updateTaskAsync(task) {
-        const options = {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body:  JSON.stringify(task),
-        };
-        const url = `${TODO_URL}/${task.id}`;
-        const response = await fetch(url, options);
-        const data = await response.json();
-        return data;
-    }
-
-    async function deleteTaskAsync(id) {
-        
-    }
+export async function deleteTaskAsync(id) {
+    const url = `${TODO_URL}/${task.id}`;
+    const res = await fetch(url, {
+        method: 'DELETE',
+    })
+    const data = await response.json();
+    return data;
+}
 
 
-    const tasks = await getTasksAsync();
-    console.log('List', tasks)
+    // const tasks = await getTasksAsync();
+    // console.log('List', tasks)
 
-    const task = await getTaskAsync(2);
-    console.log('Single task', task);
+    // const task = await getTaskAsync(2);
+    // console.log('Single task', task);
 
     // const createdTask = await createTaskAsync(
     //     {
@@ -187,10 +76,5 @@ async function app() {
     // )
     // console.log('updatedTask', updatedTask);
 
-    const deleted = await deleteTaskAsync(2);
-    console.log(delete);
-
-    
-}
-
-app();
+    // const deleted = await deleteTaskAsync(2);
+    // console.log(delete);
