a47a4cc316e6df7a6a2beb47dc5c1fc9b46c7032
i.maslakoff <igormaslakoff@gmail.com>
2021-03-26 20:49:21 +0300

    chagnge
    Co-authored-by: Dmitri Urbanovich <dimaurbanovich@users.noreply.github.com>
    Co-authored-by: A-Dulevich <A-Dulevich@users.noreply.github.com>
    Co-authored-by: Ivantsov-aa <Ivantsov-aa@users.noreply.github.com>
    Co-authored-by: Elizaveta-Battalova <Elizaveta-Battalova@users.noreply.github.com>

--- src/js/request.js
+++ src/js/request.js
@@ -1,5 +1,8 @@
+import 'regenerator-runtime/runtime';
+
 async function main() {
 
+    
     const URL = 'https://jsonplaceholder.typicode.com/posts'
 
     const response = await fetch(`${URL}/1`);
@@ -25,7 +28,7 @@ async function main() {
     console.log(`POST data`, newRecordData);
 }
 
-main();
+// main();
 
 
 
@@ -95,3 +98,99 @@ main();
 // .catch(err => {
 //     console.log('Error: ' + err.message);
 // })
+
+
+function getTasks() {
+    const getTask = fetch('http://localhost:3000/todos');
+    getTask
+    .then(promise => {
+        console.log(promise)
+        return promise.json();
+    })
+    .then(promise => {
+        console.log(promise);
+    })
+    .catch(err => {
+        console.log('Error:', err)
+    })
+}
+
+const TODO_URL = 'http://localhost:3000/todos';
+async function app() {
+    
+    async function getTasksAsync() {
+        const response = await fetch(TODO_URL);
+        const data = await response.json();
+        return data;
+    }
+
+    async function getTaskAsync(id) {
+        const res = await fetch(`${TODO_URL}/${id}`);
+        const dataRes = await res.json();
+        return dataRes;
+    }
+
+    async function createTaskAsync(task) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+             
+            body:  JSON.stringify(task),
+        };
+        const newRecordResponse = await fetch(TODO_URL, options);
+        const dataRes = await newRecordResponse.json();
+        return dataRes;
+    }
+
+    async function updateTaskAsync(task) {
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body:  JSON.stringify(task),
+        };
+        const url = `${TODO_URL}/${task.id}`;
+        const response = await fetch(url, options);
+        const data = await response.json();
+        return data;
+    }
+
+    async function deleteTaskAsync(id) {
+        
+    }
+
+
+    const tasks = await getTasksAsync();
+    console.log('List', tasks)
+
+    const task = await getTaskAsync(2);
+    console.log('Single task', task);
+
+    // const createdTask = await createTaskAsync(
+    //     {
+    //         "text": "Created from JS",
+    //         "completed": false,
+    //     }
+    // )
+    // console.log(createdTask);
+
+
+    // const updatedTask = await updateTaskAsync(
+    //     {
+    //         "text": "Updated from JS",
+    //         "completed": true,
+    //         id: 5,
+    //     }
+    // )
+    // console.log('updatedTask', updatedTask);
+
+    const deleted = await deleteTaskAsync(2);
+    console.log(delete);
+
+    
+}
+
+app();
